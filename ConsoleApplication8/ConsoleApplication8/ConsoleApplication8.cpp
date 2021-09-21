1777de4ffc2eb8c40aeea099013bb7a02d24262b
ELLIOT BORDREZ <elliot.bordrez@laposte.net>
2021-09-21 11:53:52 +0200

    update

--- ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
@@ -1,20 +1,34 @@
 // ConsoleApplication8.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<iostream>
+#include<conio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int age, sexe,  nbcinema, FastFood, nbFastFood, Sport;
+
+	printf("Entrer votre age : ");
+	scanf("%d", &age);
+
+	printf("Quelle est votre sexe ? Entrez 1 pour fille, Entrez 2 pour garcon : ");
+	scanf("%d", &sexe);
+
+	printf("Nombre de fois ou vous allez au cinema par mois : ");
+	scanf("%d", &nbcinema);
+
+	printf("Quelle Fast Food preferez vous ? Entrez 1 pour Quick,  Entrez 2 pour MacDo : ");
+	scanf("%d", &FastFood);
+
+	printf("Combien de fois allez vous au FastFood par moi ? : ");
+	scanf("%d", &nbFastFood);
+
+	printf("Pratiquez vous un Sport ? Si oui , lequel , 1 pour le foot, 2 pour le tennis, 3 pour le basket, 4 pour le judo, 5 pour Natation : ");
+	scanf("%d", &Sport);
+
+	return 0;
+
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
